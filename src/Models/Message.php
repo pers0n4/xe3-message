3f4c584fc00f9b01f672e209dd1790d7e7a10b8b
Dong-Young Kim <31337.persona@gmail.com>
2020-10-23 17:41:47 +0900

    feat(model): add message model

---
+++ src/Models/Message.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pers0n4\XePlugin\Message\Models;
+
+use Xpressengine\Database\Eloquent\DynamicModel;
+use Xpressengine\User\Models\User;
+
+class Message extends DynamicModel
+{
+    protected $table = 'messages';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'receiver_id',
+        'sender_id',
+        'title',
+        'content',
+        'is_readed',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function sender()
+    {
+        return $this->hasOne(User::class, 'id', 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->hasOne(User::class, 'id', 'receiver_id');
+    }
+}
