2875869fd538925c31cb637318bbe4b53bc1062f
Dong-Young Kim <31337.persona@gmail.com>
2020-10-25 20:23:53 +0900

    feat(view): add index view

--- views/index.blade.php
+++ views/index.blade.php
@@ -1,1 +1,22 @@
-<div class="title">{{ $title }}</div>
+@if ($messages->count())
+<table class="table is-fullwidth is-hoverable messages">
+    <thead>
+        <tr>
+            <th scope="col" class="messages__sender">발신</th>
+            <th scope="col" class="messages__content">내용</th>
+            <th scope="col" class="messages__date">날짜</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($messages as $message)
+        <tr>
+            <td>{{ $message->sender->display_name }}</td>
+            <td>{{ $message->content }}</td>
+            <td>{{ $message->created_at }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@else
+No messages
+@endif
