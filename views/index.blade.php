0a817061d32d6e0412610c9d382f40562a7a0183
Dong-Young Kim <31337.persona@gmail.com>
2020-10-25 23:36:04 +0900

    fix(assets): fix table layout

--- views/index.blade.php
+++ views/index.blade.php
@@ -2,7 +2,7 @@
 <table class="table is-fullwidth is-hoverable messages">
     <thead>
         <tr>
-            <th scope="col" class="messages__sender">발신</th>
+            <th scope="col">발신</th>
             <th scope="col" class="messages__content">내용</th>
             <th scope="col" class="messages__date">날짜</th>
         </tr>
@@ -10,9 +10,9 @@
     <tbody>
         @foreach ($messages as $message)
         <tr>
-            <td>{{ $message->sender->display_name }}</td>
-            <td>{{ $message->content }}</td>
-            <td>{{ $message->created_at }}</td>
+            <td><span class="messages__sender">{{ $message->sender->display_name }}</span></td>
+            <td class="messages__content">{{ $message->content }}</td>
+            <td class="messages__date">{{ $message->created_at }}</td>
         </tr>
         @endforeach
     </tbody>
